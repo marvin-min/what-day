8d6bdf6db305bbead46f7c39374724876dd2137e
marvin.min <marvin.min@activenetwork.com>
2020-11-10 10:41:10 +0800

    update code

--- lib/what/day.rb
+++ lib/what/day.rb
@@ -3,6 +3,12 @@ require "what/day/version"
 module What
   module Day
     class Error < StandardError; end
-    # Your code goes here...
+    def calc(date)
+      if date.nil? && !date.is_a?(Time)
+        raise Error.new("param 'date' should be a Time type")
+      else
+        date.strftime('%A')
+      end
+    end
   end
 end
